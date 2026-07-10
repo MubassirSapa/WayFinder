523d6c52a25de097061244eb0cddf8161512ab5e
MohHasan1 <hemailhasan88@gmail.com>
2026-07-10 11:12:59 -0400

    Updated

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["badge-arcade-donna-commercial.trycloudflare.com"],
 };
 
 export default withPayload(nextConfig);
